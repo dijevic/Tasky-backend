fd544b8032253062320f8b62799297837654247d
[diego] <[dielmac7@gmail.com]>
2022-01-27 12:15:48 -0300

    backend deployed

--- DB/db.config.js
+++ DB/db.config.js
@@ -3,19 +3,19 @@ const { Sequelize } = require('sequelize')
 
 
 
-const sequelize = new Sequelize('todo', 'root', null, {
-    host: 'localhost',
-    dialect: 'mariadb',
-
-
-});
-
-// const sequelize = new Sequelize('bcy6kjq7p7lcjmoivkih', 'untnjcnnl2p5u1u1', '1qozASuovYgka8ztZZCk', {
-//     host: 'bcy6kjq7p7lcjmoivkih-mysql.services.clever-cloud.com',
-//     dialect: 'mysql',
+// const sequelize = new Sequelize('todo', 'root', null, {
+//     host: 'localhost',
+//     dialect: 'mariadb',
 
 
 // });
+
+const sequelize = new Sequelize('bcy6kjq7p7lcjmoivkih', 'untnjcnnl2p5u1u1', '1qozASuovYgka8ztZZCk', {
+    host: 'bcy6kjq7p7lcjmoivkih-mysql.services.clever-cloud.com',
+    dialect: 'mysql',
+
+
+});
 
 
 
